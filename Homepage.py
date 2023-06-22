9069d25e9ca8280d081d73700265c451e0c204a9
Ram <a.ramachandran.2002@gmail.com>
2023-06-22 21:13:44 +0530

    homepage updated

--- Homepage.py
+++ Homepage.py
@@ -5,3 +5,10 @@ st.set_page_config(
 )
 
 st.title("Welcome to the home page of the Multiapp")
+st.write(""""
+##### Here these are the following apps are available to you to use
+    1. A current stock growth of Google is being presented
+    2. A web app to show in detail about the constituents of a given DNA 
+    3. A place where we can find your favourite NBA and their stats
+    4. A place where your can find the details about NFL players
+""")
